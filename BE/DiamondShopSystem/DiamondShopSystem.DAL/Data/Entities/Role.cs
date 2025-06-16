4fe27f485a85d5df10cba3c8da3a4ce6ed6622b5
Nguyễn Anh Quân <144054352+BlueBerryD42@users.noreply.github.com>
2025-06-16 15:14:22 +0700

    Creating entities for dbcontext

---
+++ BE/DiamondShopSystem/DiamondShopSystem.DAL/Data/Entities/Role.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DiamondShopSystem.DAL.Data.Entities
+{
+    public enum RoleType
+    {
+        Head_Official_Admin,
+        Store_Manager,
+        Sale_Staff,
+        Delivery_Staff
+    }
+
+    [Table("Roles")]
+    public class Role
+    {
+        [Key]
+        [Required]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public Guid Id { get; set; }
+
+        [Required]
+        [Column("role_name")]
+        public RoleType RoleName { get; set; }
+    }
+}
