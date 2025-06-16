11a84fef3695f9e31beb0f1509c70ff48e4cd926
Nguyễn Anh Quân <144054352+BlueBerryD42@users.noreply.github.com>
2025-06-16 17:46:46 +0700

    Feature/entities (#7)
    * Creating entities for dbcontext
    
    * Db entities update
    
    * update db appsetting

---
+++ BE/DiamondShopSystem/DiamondShopSystem.DAL/Data/Entities/Warranties.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiamondShopSystem.DAL.Data.Entities
+{
+    internal class Warranties
+    {
+    }
+}
