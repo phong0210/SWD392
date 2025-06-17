aa740457ac745f1218ca017d52322647867ed663
Nguyễn Anh Quân <144054352+BlueBerryD42@users.noreply.github.com>
2025-06-17 17:14:44 +0700

    Be (#9)
    * Add necessary folders
    
    * Add dependency, packages
    
    * Setup docker support
    
    * Setup docker support
    
    * Add Copy Local
    
    * Creating entities for dbcontext
    
    ---------
    
    Co-authored-by: phong0210 <Phong01062019@gmail.com>

---
+++ BE/DiamondShopSystem/DiamondShopSystem.DAL/Data/Entities/LoyaltyPoints.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace DiamondShopSystem.DAL.Data.Entities
+{
+    [Table("LoyaltyPoints")]
+    public class LoyaltyPoints
+    {
+        [ForeignKey("User")] 
+        [Required]
+        [Column("id")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [Column("points_earned")]
+        public int Points_earned { get; set; } = 0;
+
+        [Required]
+        [Column("points_redeemed")]
+        public int Points_redeemed { get; set; } = 0;
+
+        [Required]
+        [Column("last_updated")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}", ApplyFormatInEditMode = true)]
+        public DateTime LastUpdated { get; set; } = DateTime.UtcNow;
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ BE/DiamondShopSystem/DiamondShopSystem.DAL/Data/Entities/Order.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DiamondShopSystem.DAL.Data.Entities
+{
+
+    public enum Status
+    {
+        Pending,     // Order is pending and awaiting processing
+        Processing,  // Order is currently being processed
+        Completed,   // Order has been completed successfully
+        Cancelled,   // Order has been cancelled by the user or system
+        Refunded     // Order has been refunded to the user
+    }
+
+    [Table("Orders")]
+    public class Order
+    {
+        [Key]
+        [Required]
+        [Column("id")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [ForeignKey("User")]
+        [Column("id")]
+        public Guid UserId { get; set; }
+
+        [Column("promotion_id")]
+        public Guid? PromotionId { get; set; }
+
+        [Required]
+        [Column("total_amount")]
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:C}", ApplyFormatInEditMode = true)]
+        public float TotalAmount { get; set; }
+
+        [Required]
+        [Column("order_date")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}", ApplyFormatInEditMode = true)]
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+
+        [Column("vip_applied")]
+        public bool VipApplied { get; set; } = false;
+
+        [Column("loyalty_points_used")]
+        public int LoyaltyPointsUsed { get; set; } = 0;
+
+        [Column("status")]
+        [Required]
+        public Status Status { get; set; } = Status.Pending;
+
+        [Column("sale_staff_id")]
+        [Required]
+        public Guid? SaleStaffId { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
