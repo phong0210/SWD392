cb66ecd88966475b95fa5a15181b035972186269
BlueBerryD42 <syrexd42@gmail.com>
2025-06-17 19:05:12 +0700

    Unit Of Work, Generic Repo, Factory, JWT, Program setup

---
+++ BE/DiamondShopSystem/DiamondShopSystem.BLL/Utils/IJWTUtil.cs
@@ -0,0 +1,9 @@
+﻿using DiamondShopSystem.DAL.Data.Entities;
+
+namespace DiamondShopSystem.BLL.Utils
+{
+        public interface IJWTUtil
+        {
+            string GenerateJwtToken(User user, Tuple<string, Guid> tuple, bool flag);
+        }
+}
---
+++ BE/DiamondShopSystem/DiamondShopSystem.DAL/Repositories/Impelements/GenericRepository.cs
@@ -0,0 +1,29 @@
+﻿using DiamondShopSystem.DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DiamondShopSystem.DAL.Repositories.Impelements
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public async Task<TEntity?> GetByIdAsync(object id) => await _dbSet.FindAsync(id);
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync() => await _dbSet.ToListAsync();
+
+        public async Task AddAsync(TEntity entity) => await _dbSet.AddAsync(entity);
+
+        public void Update(TEntity entity) => _dbSet.Update(entity);
+
+        public void Remove(TEntity entity) => _dbSet.Remove(entity);
+    }
+}
